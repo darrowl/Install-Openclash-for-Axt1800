7e32aa67e8a82f734339ceb802062751a36726e5
darrowl <darrowl@163.com>
2023-04-08 11:38:42 +0800

    Update FT.js

--- FT.js
+++ FT.js
@@ -1,18 +1,10 @@
 /***********************************
 
-> 應用名稱：FT中文網
-> 軟件版本：6.11.45
-> 下載地址：https://apps.apple.com/cn/app/id443870811
-> 腳本作者：群友投稿
-> 微信賬號：墨魚手記
-> 更新時間：2022-03-06
-> 通知頻道：https://t.me/ddgksf2021
-> 問題反饋：https://t.me/ddgksf2013_bot
-> 特別說明：本腳本僅供學習交流使用，禁止轉載售賣
- 
+> 更新時間：2023-03-06
+
 [rewrite_local]
 
-# ～ FT中文網解鎖會員權限（2022-03-06）@ddgksf2013
+# ～ FT中文
 ^https?:\/\/user\.ftmailbox\.cn\/ios-receipt-validation\.php.* url script-response-body https://raw.githubusercontent.com/darrowl/Install-Openclash-for-Axt1800/main/FT.js
 
 [mitm] 
